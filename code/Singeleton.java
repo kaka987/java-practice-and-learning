9fec2d88df2d922c4785031bac44c6c39a7962d8
Young <zxf.0810@163.com>
2017-10-24 12:25:42 +0800

    Create Singeleton.java

---
+++ code/Singeleton.java
@@ -0,0 +1,23 @@
+//JAVA单例
+
+package com.young.singleton;
+
+public class Singleton {
+    private static Singleton instant;
+    private Singleton() {}
+
+    public static Singleton getInstant() {
+        if (instant == null) {
+            instant = new Singleton();
+        }
+        return instant;
+    }
+}
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstant();
+        Singleton s2 = Singleton.getInstant();
+        System.out.println(s1 == s2);
+    }
+}
